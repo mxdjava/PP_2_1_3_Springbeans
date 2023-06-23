dd94e15f0389d3fa45cf73e5def1c4fab135897a
mxdjava <charismaticdrugger@gmail.com>
2023-06-23 18:06:57 +0600

    Не знаю почему, но работает ))

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-
+    @Bean
     public Long getTime() {
         return nanoTime;
     }
